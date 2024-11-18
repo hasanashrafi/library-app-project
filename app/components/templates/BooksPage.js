7917b654986dac9e32b3677adadcae4b2687e785
Hasan Ashrafi <yasashrafi@gmail.com>
2024-11-18 11:57:53 +0330

    create books page component

---
+++ app/components/templates/BooksPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BooksPage() {
+    return (
+        <div className='min-h-screen p-5'>
+            BooksPage
+        </div>
+    )
+}
+
+export default BooksPage
